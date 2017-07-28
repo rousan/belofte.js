5333cd9052f7d6fb77fa7d66f0cf7b9f1859b00f
Ariyan Khan <ariyankhan46@gmail.com>
2017-07-28 15:06:23 +0530

    Updated test

--- test/test.js
+++ test/test.js
@@ -1,21 +1,20 @@
 
-
 var Belofte = require("../dist/belofte");
 
 var promise = new Belofte.Promise(function (resolve, reject) {
-   Belofte.runAsync(resolve, undefined, 121);
+    Belofte.runAsync(resolve, undefined, 121);
 });
 
 promise.then(function (value) {
-   console.log(value);
-   return Belofte.resolve(1000);
+    console.log(value);
+    return Belofte.resolve(1000);
 }).then(function (value) {
     console.log(value);
-   var deferred = Belofte.defer();
-   Belofte.runAsync(function () {
-      deferred.resolve(122);
-   });
-   return deferred.promise;
+    var deferred = Belofte.defer();
+    Belofte.runAsync(function () {
+        deferred.resolve(122);
+    });
+    return deferred.promise;
 }).catch(function (err) {
     console.log(err);
 });
