7b1a2ab5d0cd6dfb42d46c0c95b6a0468f5a29e1
Rousan Ali <rousanali786@gmail.com>
2018-01-28 20:59:20 +0530

    Removed unneccessary files

--- spec/promises-aplus.spec.js
+++
@@ -1,6 +0,0 @@
-var promisesAPlusTests = require("promises-aplus-tests");
-var belofteJS = require('../src/belofte');
-
-describe("Promises/A+ Tests", function () {
-  promisesAPlusTests.mocha(belofteJS);
-});
